068aedcac39ba09da1671133bd1a9e7be613cd91
macadamia2002 <69433090+macadamia2002@users.noreply.github.com>
2020-08-09 11:30:48 -0700

    check_dup
    a function that checks for dups in the numbers list

---
+++ check_dup.py
@@ -0,0 +1,20 @@
+def check_dup(nums):
+  seen = set()
+  for num in nums:
+    if num in seen:
+      return True
+    seen.add(num)
+  return False
+
+
+# Test
+t1 = [1,2,3,4,5,6]
+assert t1 === False, "should be False, no dups"
+
+t2 = []
+assert t2 === False, "should be False, no dups"
+
+t3 = [1,2,45,88,1]
+assert t3 === False, "should be True"
+      
+  
